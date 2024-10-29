cfb6b0668d0cdec66280f458847ff8399560be55
Amedeo1997 <sirjaizz@gmail.com>
2024-10-29 20:26:24 +0100

    Life in weeks done

---
+++ 05-Js Exercises/life-in-weeks/script.js
@@ -0,0 +1,11 @@
+function lifeInWeeks(age) {
+    let maxLife = 90;
+    let days = (maxLife - age) * 365;
+    let weeks = (maxLife - age) * 52;
+    let months = (maxLife - age) * 12;
+
+    console.log('you have ' + days + ' days, ' + weeks + ' weeks, ' + months + ' months left.');
+
+}
+
+lifeInWeeks(89);
