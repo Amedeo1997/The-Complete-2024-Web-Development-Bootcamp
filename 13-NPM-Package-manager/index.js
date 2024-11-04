e24f8cd7f497f707c4ccb5e1082d9d48fbb5f40c
Amedeo1997 <sirjaizz@gmail.com>
2024-11-04 20:26:58 +0100

    Generated random superhero name with superhero package

--- 13-NPM-Package-manager/index.js
+++ 13-NPM-Package-manager/index.js
@@ -1,6 +1,8 @@
 //var generateName = require('sillyname');
-
+import {randomSuperhero} from 'superheroes';
 import generateName from 'sillyname';
 var sillyName = generateName();
+var heroName = randomSuperhero();
 
 console.log(`My name is ${sillyName}.`);
+console.log(`I am a superhero and my name is ${heroName}!`);
