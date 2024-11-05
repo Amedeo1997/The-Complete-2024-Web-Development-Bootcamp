61338a5081688ba8881ba6601f8e72ac4d1a1f9d
Amedeo1997 <sirjaizz@gmail.com>
2024-11-05 18:26:21 +0100

    created about contact and home page

--- 15-Express-js-introduction/index.js
+++ 15-Express-js-introduction/index.js
@@ -10,6 +10,10 @@ app.get('/about', (req, res) => {
     res.send('<h1>About Me</h1>');
 });
 
+app.get('/contact', (req, res) => {
+    res.send('<h1>Contact Me</h1>');
+});
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
