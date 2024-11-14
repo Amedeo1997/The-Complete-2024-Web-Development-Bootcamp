baea202b9a39dbd0d929068d0fc493c759cd4e74
Amedeo1997 <sirjaizz@gmail.com>
2024-11-14 17:28:05 +0100

    done

--- 23-Axios/index.js
+++ 23-Axios/index.js
@@ -11,6 +11,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // Step 1: Make sure that when a user visits the home page,
 //   it shows a random activity.You will need to check the format of the
 //   JSON data from response.data and edit the index.ejs file accordingly.
+
+
 app.get("/", async (req, res) => {
   try {
     const response = await axios.get("https://bored-api.appbrewery.com/random");
@@ -26,6 +28,31 @@ app.get("/", async (req, res) => {
 
 app.post("/", async (req, res) => {
   console.log(req.body);
+  const { type, participants } = req.body;
+  try {
+    const response = await axios.get(
+      "https://bored-api.appbrewery.com/filter",
+      {
+        params: {
+          type,
+          participants,
+        },
+      }
+    );
+    const result = response.data;
+    if (result.error) {
+      res.render("index.ejs", {
+        error: "No activities that match your criteria.",
+      });
+    } else {
+      res.render("index.ejs", { data: result });
+    }
+  } catch (error) {
+    console.error("Failed to make request:", error.message);
+    res.render("index.ejs", {
+      error: error.message,
+    });
+  }
 
   // Step 2: Play around with the drop downs and see what gets logged.
   // Use axios to make an API request to the /filter endpoint. Making
